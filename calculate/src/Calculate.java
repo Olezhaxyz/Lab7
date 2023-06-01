9b0fa2f61093e9a1c5f9571e7bc2b85dd90a4097
Olezhaxyz <trup.maks@gmail.com>
2023-06-01 13:09:41 +0300

    First commit

---
+++ calculate/src/Calculate.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Calculate {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double ans;
+        char op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Напишите 2 числа: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nВыберите действие (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        switch (op) {
+            case '+' -> ans = num1 + num2;
+            case '-' -> ans = num1 - num2;
+            case '*' -> ans = num1 * num2;
+            case '/' -> ans = num1 / num2;
+            default -> {
+                System.out.printf("Ошибка! Неправльный оператор");
+                return;
+            }
+        }
+        System.out.print("\nРезультат:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+    }
+}
